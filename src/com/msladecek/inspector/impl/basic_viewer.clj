8745a246ad7d3a1fa33104bb48f54136ef7b9cd9
msladecek <martin.sladecek@gmail.com>
2025-10-01 11:25:20 +0200

    allow switching viewer representations

--- src/com/msladecek/inspector/impl/basic_viewer.clj
+++ src/com/msladecek/inspector/impl/basic_viewer.clj
@@ -1,7 +1,7 @@
 (ns com.msladecek.inspector.impl.basic-viewer
   (:require
     [clojure.datafy :refer [datafy]]
-    [clojure.pprint :refer [pprint print-table]]
+    [clojure.pprint :as pprint]
     [clojure.spec.alpha :as spec]
     [com.msladecek.inspector.protocols :as proto]))
 
@@ -12,12 +12,44 @@
 
 (spec/def ::table (spec/coll-of map?))
 
-(defn draw-state-to-screen [{:keys [views selected-view-idx]}]
+(defn default-representation [data]
+  (if (spec/valid? ::table data)
+    :table
+    :default))
+
+(defmulti print-data :representation)
+
+(defmethod print-data :table [view]
+  (let [all-keys (->> (:data view)
+                      (map keys)
+                      (apply concat)
+                      (into #{})
+                      (sort))]
+    (pprint/print-table all-keys (:data view))))
+
+(defmethod print-data :default [view]
+  (-> (:data view)
+      (datafy)
+      (pprint/pprint)))
+
+(def next-representation
+  (let [representations (keys (methods print-data))]
+    (->> (map vector representations (next (cycle representations)))
+         (into {}))))
+
+(def previous-representation
+  (->> next-representation
+       (map (fn [[r1 r2]] [r2 r1]))
+       (into {})))
+
+(defn print-state [{:keys [views selected-view-idx]}]
+  ;; TODO: fix broken output when a bunch of data is submitted at once
   (print reset-seq clear-screen-seq (move-cursor-seq 1 1))
-  (let [data (get views selected-view-idx)]
-    (println (format "view %s/%d"
+  (let [view (get views selected-view-idx)]
+    (println (format "view %s/%d%s"
                      (if (nil? selected-view-idx) "-" (str (inc selected-view-idx)))
-                     (count views)))
+                     (count views)
+                     (if view (str " as " (-> view :representation name)) "")))
     (cond
       (nil? selected-view-idx)
       (println "no view selected")
@@ -26,29 +58,26 @@
       (println (format "invalid selected view %d, only %d views available"
                        (inc selected-view-idx) (count views)))
 
-      (spec/valid? ::table data)
-      (let [all-keys (->> data
-                          (map keys)
-                          (apply concat)
-                          (into #{})
-                          sort)]
-        (print-table all-keys data))
-
       :else
-      (pprint (datafy data)))))
+      (try
+        (print-data view)
+        (catch Exception _
+          (println (format "ERROR: failed to represent the data as %s, displaying instead the default representation:"
+                           (-> view :representation name)))
+          (print-data (assoc view :representation :default)))))))
 
 (defrecord BasicViewer [state]
   proto/Viewer
   (display [_ data]
     (swap! state (fn [{:keys [views] :as state}]
                    (-> state
-                       (update :views conj data)
+                       (update :views conj {:data data :representation (default-representation data)})
                        (assoc :selected-view-idx (count views)))))
-    (draw-state-to-screen @state)
+    (print-state @state)
     true)
 
   (on-key [_ key]
-    (when (#{\H \L} key)
+    (when (#{\H \L \[ \]} key)
       (swap! state (fn [{:keys [selected-view-idx views] :as state}]
                      (cond
                        (nil? selected-view-idx)
@@ -60,14 +89,20 @@
                        (and (= \L key) (< selected-view-idx (dec (count views))))
                        (update state :selected-view-idx inc)
 
+                       (= \[ key)
+                       (update-in state [:views selected-view-idx :representation] previous-representation)
+
+                       (= \] key)
+                       (update-in state [:views selected-view-idx :representation] next-representation)
+
                        :else
                        state)))
-      (draw-state-to-screen @state))))
+      (print-state @state))))
 
 (defn make-basic-viewer
   ([]
    (let [viewer (->BasicViewer (atom {:views []}))]
-     (draw-state-to-screen @(:state viewer))
+     (print-state @(:state viewer))
      viewer))
    ([_opts]
     (make-basic-viewer)))
