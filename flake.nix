839215ed4a932079aeaf8c913ad1ff3a70beff59
msladecek <martin.sladecek@gmail.com>
2025-10-09 01:33:44 +0200

    switch to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/25.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,55 +15,61 @@
     inputs@{
       self,
       nixpkgs,
-      flake-utils,
+      flake-parts,
       treefmt-nix,
       lefthook,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        dependencies = with pkgs; [
-          clojure
-          clj-kondo
-        ];
-        treefmtEval = treefmt-nix.lib.evalModule pkgs {
-          projectRootFile = "flake.nix";
-          programs.nixfmt.enable = true;
-        };
-        lefthook-check = lefthook.lib.${system}.run {
-          src = ./.;
-          config = {
-            colors = false;
-            pre-commit.fail_on_changes = "always";
-            pre-commit.commands = {
-              clj-kondo = {
-                run = "clj-kondo --repro --lint {staged_files}";
-                glob = [
-                  "*.cljc"
-                  "*.clj"
-                ];
-              };
-              nix-fmt = {
-                run = "nix fmt {staged_files}";
-                glob = "*.nix";
-              };
-            };
-            output = [
-              "execution"
-              "failure"
-            ];
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          dependencies = with pkgs; [
+            clojure
+            clj-kondo
+          ];
+          treefmtEval = treefmt-nix.lib.evalModule pkgs {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
           };
+          lefthook-check = lefthook.lib.${system}.run {
+            src = ./.;
+            config = {
+              colors = false;
+              pre-commit.fail_on_changes = "always";
+              pre-commit.commands = {
+                clj-kondo = {
+                  run = "clj-kondo --repro --lint {staged_files}";
+                  glob = [
+                    "*.cljc"
+                    "*.clj"
+                  ];
+                };
+                nix-fmt = {
+                  run = "nix fmt {staged_files}";
+                  glob = "*.nix";
+                };
+              };
+              output = [
+                "execution"
+                "failure"
+              ];
+            };
+          };
+        in
+        {
+          checks = { inherit lefthook-check; };
+          devShells.default = pkgs.mkShell {
+            inherit (self.checks.${system}.lefthook-check) shellHook;
+            buildInputs = dependencies ++ [ pkgs.rlwrap ];
+          };
+          formatter = treefmtEval.config.build.wrapper;
         };
-      in
-      {
-        checks = { inherit lefthook-check; };
-        devShells.default = pkgs.mkShell {
-          inherit (self.checks.${system}.lefthook-check) shellHook;
-          buildInputs = dependencies ++ [ pkgs.rlwrap ];
-        };
-        formatter = treefmtEval.config.build.wrapper;
-      }
-    );
+    };
 }
